a114b4bbecc7ee95e5b55971d0b35d357719a20d
Elyor <elyor@outlook.com>
2015-10-05 01:53:03 +0500

    Deploy

--- out/artifacts/heroku_marketing_war_exploded/WEB-INF/classes/scripts/delete.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM public.users;
-DELETE FROM public.devices;
-DELETE FROM public.products;
-DELETE FROM public.news;
-DELETE FROM public.companies;
--- out/artifacts/heroku_marketing_war_exploded/WEB-INF/classes/scripts/drop.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE public.users;
-DROP TABLE public.devices;
-DROP TABLE public.products;
-DROP TABLE public.news;
-DROP TABLE public.companies;
